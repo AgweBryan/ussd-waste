7160df22d2c141e703ffa4bf7d9dcf6091fb741a
AgweBryan <agwebryanmunakwa@gmail.com>
2022-11-15 23:30:54 +0100

    change route method

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def hellow_world():
     return 'first page'
 
 
-@app.route('/ussd', methods = ['POST', 'GET'])
+@app.route('/ussd', methods = ['POST'])
 def ussd():
     # Read the variables sent via POST from our API
     session_id = request.values.get('sessionId')
