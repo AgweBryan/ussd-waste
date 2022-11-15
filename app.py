fa79729fd9e92da97085c92fbbdf54871d2b1197
AgweBryan <agwebryanmunakwa@gmail.com>
2022-11-15 20:02:28 +0100

    testing

--- app.py
+++ app.py
@@ -98,143 +98,144 @@ def hellow_world():
     value1 = '1'
     value2 = '2'
     sum = int(value1) + int(value2)
-    return sum
+    return 'first page'
 
 
 @app.route('/ussd', methods = ['GET', 'POST'])
 def ussd():
     # Read the variables sent via POST from our API
-    session_id = request.values.get('sessionId')
-    service_code = request.values.get('serviceCode')
-    phone_number = request.args.get('phoneNumber')
-    text = request.args.get('text') # The first begins with CON and the last with END
-    # fetch user
-    user = find_user(phone_number)
+    # session_id = request.values.get('sessionId')
+    # service_code = request.values.get('serviceCode')
+    # phone_number = request.args.get('phoneNumber')
+    # text = request.args.get('text') # The first begins with CON and the last with END
+    # # fetch user
+    # user = find_user(phone_number)
 
-    x = text.split('*')
-    y = len(x)
+    # x = text.split('*')
+    # y = len(x)
 
-    print('The Value of x: ', x)
+    # print('The Value of x: ', x)
 
 
-    if user:
-        if text == '' or x[y-1] == '99':
-            # Main menu
-            response = 'CON Welcome back ' + user.name + '! How can we assist you\n\n'
-            response += '1. Request pickup\n'
-            response += '2. Record transaction\n'
-            response += '3. Check SOSOCASH balance\n'
-            response += '4. Withdraw funds\n'
-            response += '5. Submit insurance claim\n'
-            response += '6. Change PIN\n'
+    # if user:
+    #     if text == '' or x[y-1] == '99':
+    #         # Main menu
+    #         response = 'CON Welcome back ' + user.name + '! How can we assist you\n\n'
+    #         response += '1. Request pickup\n'
+    #         response += '2. Record transaction\n'
+    #         response += '3. Check SOSOCASH balance\n'
+    #         response += '4. Withdraw funds\n'
+    #         response += '5. Submit insurance claim\n'
+    #         response += '6. Change PIN\n'
          
 
-        elif text == '1':
-            # User selected request pickup
-            response = 'Enter Area'
+    #     elif text == '1':
+    #         # User selected request pickup
+    #         response = 'Enter Area'
 
-        elif y == 2 and x[0] == '1':
-            response = 'Enter waste type'
+    #     elif y == 2 and x[0] == '1':
+    #         response = 'Enter waste type'
         
-        elif y == 3 and x[0] == '1':
-            response = 'Enter Approximate quantity in kgs'
-        elif y == 4 and x[0] == '1':
-            response = 'Thank you. We are processing your request\n\n'
-            response += '99. Main menu'
+    #     elif y == 3 and x[0] == '1':
+    #         response = 'Enter Approximate quantity in kgs'
+    #     elif y == 4 and x[0] == '1':
+    #         response = 'Thank you. We are processing your request\n\n'
+    #         response += '99. Main menu'
                
-    else:
-        if text == '':
-            response = 'CON Welcome to SOSOCARE\n\n'
-            response += '1. Register\n'
-            response += '2. Terms and Conditions'
-        elif text == '1':
-            # if user does not exist, register user
-            response = 'CON Register on SOSOCARE.\n Select user type\n\n'
-            response += '1. Waste Collector\n'
-            response += '2. Waste Collection agent\n'
-            response += '3. Organization\n\n'
-            response += '99. Main menu'
+    # else:
+    #     if text == '':
+    #         response = 'CON Welcome to SOSOCARE\n\n'
+    #         response += '1. Register\n'
+    #         response += '2. Terms and Conditions'
+    #     elif text == '1':
+    #         # if user does not exist, register user
+    #         response = 'CON Register on SOSOCARE.\n Select user type\n\n'
+    #         response += '1. Waste Collector\n'
+    #         response += '2. Waste Collection agent\n'
+    #         response += '3. Organization\n\n'
+    #         response += '99. Main menu'
 
-        elif text == '2':
-            response = 'Thank you for choosing SOSOCARE insurance. Accept ECO medical plan terms and conditions Available through our whatsapp number +xxxxxxxx, agree that SOSOCARE can share your data with Medical service providers\n\n'
-            response += '0. Next'
+    #     elif text == '2':
+    #         response = 'Thank you for choosing SOSOCARE insurance. Accept ECO medical plan terms and conditions Available through our whatsapp number +xxxxxxxx, agree that SOSOCARE can share your data with Medical service providers\n\n'
+    #         response += '0. Next'
 
-        elif text == '2*0' or (x[y-1] == '0' and x[0] == '2'):
-            response = 'END And confirm you are between 18 and 65 years old. For enquiries kindly call +xxxxxxxxx\n\n'
-            response += '#. Previous'
+    #     elif text == '2*0' or (x[y-1] == '0' and x[0] == '2'):
+    #         response = 'END And confirm you are between 18 and 65 years old. For enquiries kindly call +xxxxxxxxx\n\n'
+    #         response += '#. Previous'
 
-        elif text == '2*0*#' or (x[y-1] == '#' and x[0] == '2'):
-            response = 'Thank you for choosing SOSOCARE insurance. Accept ECO medical plan terms and conditions Available through our whatsapp number +xxxxxxxx, agree that SOSOCARE can share your data with Medical service providers\n\n'
-            response += '0. Next'
+    #     elif text == '2*0*#' or (x[y-1] == '#' and x[0] == '2'):
+    #         response = 'Thank you for choosing SOSOCARE insurance. Accept ECO medical plan terms and conditions Available through our whatsapp number +xxxxxxxx, agree that SOSOCARE can share your data with Medical service providers\n\n'
+    #         response += '0. Next'
         
-        # Waste collector registration starts
-        elif text == '1*1':
-            response = 'Enter Name & Surname'
-        elif y == 3 and re.search('1\*1\*[a-zA-Z]', text):
-            # text == '1*1*username'
-            response = 'Enter Bank Account Number'
-        elif y == 4 and len(x[y-1]) == 9 and x[1] == '1':
-            # text == '1*1*username*bankAccount'
-            bank_account_number = x[y-1]
-            print('The user bankAccount', bank_account_number)
-            response = 'Create SOSOCARE Account PIN'
-        elif y == 5 and len(x[y-1]) == 4 and x[1] == '1':
-            # text == '1*1*username*bankAccount*pin'
-            username = x[y-3]
-            userpin = x[y-1]
-            current_user = User(id=phone_number, name=username, phone_number=phone_number, pin=userpin, userType='waste collector')
-            # add user to database
-            insert_user(current_user)
-            response = 'END Thank you. We are verifying your information\n\n'
-            response += '99. Main menu'
-        # Waste collector registration ends
+    #     # Waste collector registration starts
+    #     elif text == '1*1':
+    #         response = 'Enter Name & Surname'
+    #     elif y == 3 and re.search('1\*1\*[a-zA-Z]', text):
+    #         # text == '1*1*username'
+    #         response = 'Enter Bank Account Number'
+    #     elif y == 4 and len(x[y-1]) == 9 and x[1] == '1':
+    #         # text == '1*1*username*bankAccount'
+    #         bank_account_number = x[y-1]
+    #         print('The user bankAccount', bank_account_number)
+    #         response = 'Create SOSOCARE Account PIN'
+    #     elif y == 5 and len(x[y-1]) == 4 and x[1] == '1':
+    #         # text == '1*1*username*bankAccount*pin'
+    #         username = x[y-3]
+    #         userpin = x[y-1]
+    #         current_user = User(id=phone_number, name=username, phone_number=phone_number, pin=userpin, userType='waste collector')
+    #         # add user to database
+    #         insert_user(current_user)
+    #         response = 'END Thank you. We are verifying your information\n\n'
+    #         response += '99. Main menu'
+    #     # Waste collector registration ends
 
-        # Waste collector agent registration starts
-        elif text == '1*2':
-            response = 'Enter Name & Surname2'
-        elif y == 3 and re.search('1\*2\*[a-zA-Z]', text) and x[1] == '2':
-            # text == '1*1*username'
-            response = 'Enter Bank Account Number'
-        elif y == 4 and len(x[y-1]) == 9 and x[1] == '2':
-            # text == '1*1*username*bankAccount'
-            response = 'Create SOSOCARE Account PIN'
-        elif y == 5 and len(x[y-1]) == 4 and x[1] == '2':
-            # text == '1*1*username*bankAccount*pin'
-            username = x[y-3]
-            userpin = x[y-1]
-            current_user = User(id=phone_number, name=username, phone_number=phone_number, pin=userpin, userType='waste collector agent')
-            # add user to database
-            insert_user(current_user)
-            response = 'END Thank you. We are verifying your information\n\n'
-            response += '99. Main menu'
+    #     # Waste collector agent registration starts
+    #     elif text == '1*2':
+    #         response = 'Enter Name & Surname2'
+    #     elif y == 3 and re.search('1\*2\*[a-zA-Z]', text) and x[1] == '2':
+    #         # text == '1*1*username'
+    #         response = 'Enter Bank Account Number'
+    #     elif y == 4 and len(x[y-1]) == 9 and x[1] == '2':
+    #         # text == '1*1*username*bankAccount'
+    #         response = 'Create SOSOCARE Account PIN'
+    #     elif y == 5 and len(x[y-1]) == 4 and x[1] == '2':
+    #         # text == '1*1*username*bankAccount*pin'
+    #         username = x[y-3]
+    #         userpin = x[y-1]
+    #         current_user = User(id=phone_number, name=username, phone_number=phone_number, pin=userpin, userType='waste collector agent')
+    #         # add user to database
+    #         insert_user(current_user)
+    #         response = 'END Thank you. We are verifying your information\n\n'
+    #         response += '99. Main menu'
 
-        # Waste collector agent registration ends
+    #     # Waste collector agent registration ends
 
-        # Organization registration starts
+    #     # Organization registration starts
 
-        elif text == '1*3':
-            response = 'Enter Organization name'
-        elif y == 3 and re.search('1\*3\*[a-zA-Z]', text) and x[1] == '3':
-            # text == '1*1*organizationName'
-            response = 'Enter address'
-        elif y == 4 and x[1] == '3':
-            # text == '1*1*organizationName*address'
-            response = 'Create SOSOCARE Account PIN'
-        elif y == 5 and len(x[y-1]) == 4 and x[1] == '3':
-            # text == '1*1*organizationname*address*pin'
-            username = x[y-3]
-            userpin = x[y-1]
-            current_user = User(id=phone_number, name=username, phone_number=phone_number, pin=userpin, userType='Organization')
-            # add user to database
-            insert_user(current_user)
-            response = 'END Thank you. We are verifying your information\n\n'
-            response += '99. Main menu'    
+    #     elif text == '1*3':
+    #         response = 'Enter Organization name'
+    #     elif y == 3 and re.search('1\*3\*[a-zA-Z]', text) and x[1] == '3':
+    #         # text == '1*1*organizationName'
+    #         response = 'Enter address'
+    #     elif y == 4 and x[1] == '3':
+    #         # text == '1*1*organizationName*address'
+    #         response = 'Create SOSOCARE Account PIN'
+    #     elif y == 5 and len(x[y-1]) == 4 and x[1] == '3':
+    #         # text == '1*1*organizationname*address*pin'
+    #         username = x[y-3]
+    #         userpin = x[y-1]
+    #         current_user = User(id=phone_number, name=username, phone_number=phone_number, pin=userpin, userType='Organization')
+    #         # add user to database
+    #         insert_user(current_user)
+    #         response = 'END Thank you. We are verifying your information\n\n'
+    #         response += '99. Main menu'    
 
-        # Organization registration ends
+    #     # Organization registration ends
 
 
 
-    return response
+    # return response
+    return 'ussd path'
     
 
 
